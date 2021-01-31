2298f9c2ebb3c798a1d9f2b219368272efb9b2de
bushra <bushra.albadareen@gmail.com>
2021-01-31 20:57:53 +0200

    edit

--- lab5.js
+++ lab5.js
@@ -1,7 +1,21 @@
-var caketype = prompt("what do you want red velvet or carrot ?");
+var order =prompt ("what do you want red velvet or carrot");
 
-while (caketype !== "red velvet" && caketype !== "carrot"){
-    caketype = prompt("what do you want red velvet or carrot ?");
+while ( order != "red velvet" && order !== "carrot" ){
+    order =prompt ("what do you want red velvet or carrot");
 }
 
-document.write(caketype);
+var itemorder = ""; 
+
+if (order === "red velvet" ){
+    itemorder = " <img src= ' https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/02/red-velvet-cake-slice-2.jpg'/>"
+} else if (order === "carrot"){
+    itemorder = "<img src= 'https://i.ytimg.com/vi/EJ5AlErxLWY/maxresdefault.jpg'/>"
+}
+
+var nocake=prompt("how many cake do you want to order");
+var result = '';
+
+for (var i=0; i< nocake; i++)(
+    result = result + itemorder;
+)
+document.write (result);
